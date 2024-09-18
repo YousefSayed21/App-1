f5f58e6c41fa188f3adfe9615b7261e9979d914e
YousefSayed12 <yousefsayedmohamed123@gmail.com>
2024-09-18 04:29:50 -0700

    56

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
 import React from 'react'
-import { Link , BrowserRouter , Route , Routes } from 'react-router-dom'
-import Yousef from './nav/Yousef'
-import Sayed from './Sayed'
+
 const App = () => {
   return (
-      <BrowserRouter>
-        <nav>
-          <Link to="/">Yousef</Link>
-          <Link to="/Sayed">Sayed</Link>
-        </nav>
-        <Routes>
-            <Route path="/" element={<Yousef />} />
-            <Route path="/Sayed" element={<Sayed />} />
-        </Routes>
-
-
-      </BrowserRouter>
+    <div>App</div>
   )
 }
 
--- src/Sayed.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Sayed = () => {
-  return (
-    <div>Sayed</div>
-  )
-}
-
-export default Sayed
